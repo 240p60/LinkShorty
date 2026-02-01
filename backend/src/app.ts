5b2f81007c1423de5bc6cedaddbc6c5726ee1429
240p60 <mr.alhoev@mail.ru>
2026-02-01 21:34:42 +0300

    backend: fileURLToPath

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,10 @@ import { linksRoutes, redirectRoutes } from "@routes/index";
 import { errorHandler, notFoundHandler } from "@middleware/error.middleware";
 import { initRedis } from "@services/cache.service";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const prisma = new PrismaClient();
